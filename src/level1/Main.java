fbe50c34ae33021ec9f7d328efc44bdd804ca0d7
zino416 <thdx416@naver.com>
2023-03-21 10:00:19 +0900

    first commit

---
+++ src/level1/Main.java
@@ -0,0 +1,21 @@
+package level1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 0, b = 0;
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a * b);
+		
+	
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a * b);
+		
+	}
+
+}
